8509d233b74d476add39560470c088a561d5d25f
sr0116 <kysy0116@gmail.com>
2025-04-30 19:01:37 +0900

    추가

--- src/domain/User.java
+++ src/domain/User.java
@@ -7,10 +7,10 @@ public abstract class User {
     //1) 사업자 : 구현/ ID, Pw, 대표자명, 상호명, 연락처
  //2) 개인회원 : 구현/ ID, PW, 이름, 거주지, 연락처, 이력서
 	
-	String id;
-	String pw;
-	String tel;
-	String name;
+	private String id; // 아이디
+	private String pw; // 비밀번호
+	private String tel; // 연락처
+	private String name; // 이름(대표자명 or 개인회원 이름) -> 까지는 공통
 	
 	public User() {}
 	
@@ -20,5 +20,37 @@ public abstract class User {
 		this.tel = tel;
 		this.name = name;
 	}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
 
