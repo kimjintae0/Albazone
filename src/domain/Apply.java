66b0ef8ea38c54117a170805205e3730465fe019
manlubo <manlubo11@gmail.com>
2025-05-01 18:35:55 +0900

    apply 수정

--- src/domain/Apply.java
+++ src/domain/Apply.java
@@ -1,7 +1,33 @@
 package domain;
 
+import java.util.Date;
 
 public class Apply {
+	int gonggoNo;
+	int resumeNo;
+	Date date; // 언제
+	// 상태
+	
+	public int getGonggoNo() {
+		return gonggoNo;
+	}
 
-      
+	public void setGonggoNo(int gonggoNo) {
+		this.gonggoNo = gonggoNo;
+	}
+
+	public int getResumeNo() {
+		return resumeNo;
+	}
+
+	public void setResumeNo(int resumeNo) {
+		this.resumeNo = resumeNo;
+	}
+
+	public Apply() {}
+	public Apply(int gonggoNo, int resumeNo) {
+		this.gonggoNo = gonggoNo;
+		this.resumeNo = resumeNo;
+	}
+
 }
--- src/service/ApplyService.java
+++ src/service/ApplyService.java
@@ -1,5 +1,33 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.Apply;
+
 public class ApplyService {
 
+	// 싱글톤
+	private static ApplyService applyService = new ApplyService();
+	private ApplyService() {}
+	public static ApplyService getInstance() {
+		return applyService;
+	}
+	
+	// 리줌, 공고 정보 가져오기
+	private ResumeService resumeService = ResumeService.getInstance();
+	private GonggoService gonggoService = GonggoService.getInstance();
+	
+	
+	// 지원 리스트 생성
+	List<Apply> applyList = new ArrayList<>();
+	
+	
+	
+	
+	{
+	}
+	
+	
+	
 }
