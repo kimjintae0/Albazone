ae40fb33cf25e6dc83f8a82814e09300db852856
manlubo <manlubo11@gmail.com>
2025-05-07 21:38:43 +0900

    어플라이서비스 유저넘버 필드 추가

--- src/domain/Apply.java
+++ src/domain/Apply.java
@@ -8,6 +8,7 @@ public class Apply implements Serializable{
 	private int resumeNo;
 	private Date applyDate = new Date(); // 언제
 	private int applySitu;// [상태]  1 : 접수    2: 등록
+	private int userNo; // 지원한 알바 유저 정보
 	
 	
 	// 게터, 세터
@@ -42,14 +43,25 @@ public class Apply implements Serializable{
 	public void setApplySitu(int applySitu) {
 		this.applySitu = applySitu;
 	}
+	public int getUserNo() {
+		return userNo;
+	}
+	
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
 	
 	
 	
+	
+	
+
 	// 생성자
 	public Apply() {}
 
-	public Apply(int gonggoNo, int resumeNo) {
+	public Apply(int gonggoNo, int resumeNo, int userNo) {
 		this.gonggoNo = gonggoNo;
 		this.resumeNo = resumeNo;
+		this.userNo = userNo;
 	}
 }
--- src/service/ApplyService.java
+++ src/service/ApplyService.java
@@ -27,11 +27,6 @@ public class ApplyService {
 		return applyService;
 	}
 	
-	// 리줌, 공고 정보 가져오기
-//	private UserService userService = UserService.getInstance();
-//	private ResumeService resumeService = ResumeService.getInstance();
-
-	
 	
 	// 지원 리스트 생성
 	List<Apply> applyList = new ArrayList<>();
@@ -46,7 +41,7 @@ public class ApplyService {
 		}
 		catch(FileNotFoundException e) {
 			System.out.println("Apply : 파일을 불러올 수 없습니다. 임시 데이터셋으로 진행합니다.");
-			applyList.add(new Apply(1, 1));
+			applyList.add(new Apply(1, 1, 2));
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
@@ -55,7 +50,7 @@ public class ApplyService {
 	
 	
 	
-	
+	// ========================================= 메서드 ==============================================
 	
 	
 	// 지원하기 - 알바
@@ -72,73 +67,37 @@ public class ApplyService {
 			return;
 		}
 		for(Apply a : applyList) {
-			if(a.getGonggoNo() == gonggoNo && a.getResumeNo() == resumeNo) {
-				System.out.println("해당 이력서로 이미 지원한 공고입니다.");
+			if(a.getGonggoNo() == gonggoNo && a.getUserNo() == UserService.getInstance().getLoginUser().getUserNo()) {
+				System.out.println("이미 지원한 공고입니다.");
 				return;
 			}
 		}
 		if(!nextConfirm("해당 공고에 지원하시겠습니까?")) {
-			System.out.println("공고 지원 취소");
+			System.out.println("공고 지원이 취소되었습니다.");
 			return;
 		}
-		applyList.add(new Apply(gonggoNo, resumeNo)); // 로그인 유저의 이력서 중, 유저가 선택한 이력서의 번호 
+		applyList.add(new Apply(gonggoNo, resumeNo, UserService.getInstance().getLoginUser().getUserNo())); // 로그인 유저의 이력서 중, 유저가 선택한 이력서의 번호 
 		System.out.println("공고 지원 완료");
 		save();
 	}
 	
-	// 데이트 타입 포매터
-	SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
 	
 	// 지원내역 조회 - 알바
 	public void lookupUser() {
-		int size = 0;
-		for(Resume r : ResumeService.getInstance().resumeList) {
-			
-//			r
-			// 1. 로그인한 사용자가 작성한 이력서들 가져오기
-			
-			
-			if(r.getUserNo() == UserService.getInstance().getLoginUser().getUserNo()) {
-				for(Apply a : applyList) {
-					if(r.getResumeNo() == a.getResumeNo()) {
-						for(Gonggo g : GonggoService.getInstance().gonggoList) {
-							if(a.getGonggoNo() == g.getGonggoNo()) {
-								System.out.println("지원 시간 : " + dateFormat.format(a.getApplyDate()));
-								System.out.println("지원 상태 : " + (a.getApplySitu() == 0 ? "접수" : "읽음"));
-								System.out.println(g.toString());
-								size++;
-							}
-						}
-					}
+			boolean check = false;
+			for(Apply a : applyList) {
+				if(a.getUserNo() == UserService.getInstance().getLoginUser().getUserNo()) {
+					System.out.println("지원 시간 : " + dateFormat.format(a.getApplyDate()) + "\n지원상태 : " + (a.getApplySitu() == 0 ? "접수" : "읽음"));
+					System.out.println(GonggoService.getInstance().findGonggoBy(a.getGonggoNo()));
+					check = true;
 				}
 			}
-		}
-		if(size == 0) {
-			System.out.println("지원내역이 없습니다.");
-		}
-		
-//		// 로그인한 사용자가 이력서들
-//		List<Resume> resumes = ResumeService.getInstance().findResumeBy(UserService.getInstance().getLoginUser().getUserNo());
-//		// 해당 이력서들로 지원한 지원서들
-//		List<Apply> applies = new ArrayList<>();
-//		for(Resume r : resumes) {
-//			applies.addAll(findApplysBy(r.getResumeNo()));
-//		}
-//		// 해당 지원서들에 해당되는 공고
-		
-		
+			if(!check) {
+				System.out.println("지원내역이 없습니다.");
+			}
 	}
 	
-	// 입력 resumeNo 출력 List<Apply>
-	public List<Apply> findApplysBy(int resumeNo) {
-		List<Apply> applies = new ArrayList<>();
-		for(Apply a : applyList) {
-			if(a.getResumeNo() == resumeNo) {
-				applies.add(a);
-			}
-		}
-		return applies;
-	}
+	
 	
 	// 지원 취소
 	public void remove() {
@@ -147,15 +106,9 @@ public class ApplyService {
 		boolean check = false;
 		int input = nextInt("지원을 취소하실 공고의 번호를 입력해주세요.");
 		for(Apply a : applyList) {
-			if(a.getGonggoNo() == input) {
-				for(Resume r : ResumeService.getInstance().resumeList) {
-					if(r.getUserNo() == UserService.getInstance().getLoginUser().getUserNo()) {
-						if(a.getResumeNo() == r.getResumeNo()) {
-							removeApply = a;
-							check = true;
-						}
-					}
-				}
+			if(a.getUserNo() == UserService.getInstance().getLoginUser().getUserNo() && a.getGonggoNo() == input) {
+				removeApply = a;
+				check = true;
 			}
 		}
 		if(check = false) {
@@ -163,12 +116,13 @@ public class ApplyService {
 			return;
 		}
 		applyList.remove(removeApply);
-		System.out.println("지원이 취소되었습니다.(중복 지원시, 오래된 항목이 먼저 삭제됩니다.)");
+		System.out.println("지원이 취소되었습니다.)");
 		save();
 	}
 	
-	// 내 공고에 지원한 내역 조회 - 사업자
 	
+	
+	// 내 공고에 지원한 내역 조회 - 사업자
 	public void lookupUserOwner() {
 		int size = 0;
 		for(Gonggo g : GonggoService.getInstance().gonggoList) {
@@ -205,7 +159,50 @@ public class ApplyService {
 		save();
 	}
 	
-	// 파일로 저장하기
+	
+	
+	// 회원탈퇴시 지원내역 전체삭제 - 유저
+	public void removeAllUser() {
+		for(Apply a : applyList) {
+			if (a.getUserNo() == UserService.getInstance().getLoginUser().getUserNo()) {
+				applyList.remove(a);
+			}
+		}
+		System.out.println("유저의 지원내역이 전체 삭제되었습니다.");
+		save();
+	}
+	
+	
+	// 공고 삭제 시 해당공고 지원내역 전체삭제 - 사업자
+	public void removeAllOwner(int gonggoNo) {
+				for(Apply a : applyList) {
+					if(a.getGonggoNo() == gonggoNo) {
+						applyList.remove(a);
+					
+					}
+				}
+		System.out.println("해당 공고에 지원한 지원내역이 전체 삭제되었습니다.");
+		save();
+	}
+	
+	
+//		============================== 자체 유틸 ============================
+		// 데이트 타입 포매터
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
+		
+		
+		// 입력 resumeNo 출력 List<Apply>
+		public List<Apply> findApplysBy(int resumeNo) {
+			List<Apply> applies = new ArrayList<>();
+			for(Apply a : applyList) {
+				if(a.getResumeNo() == resumeNo) {
+					applies.add(a);
+				}
+			}
+			return applies;
+		}
+		
+		// 파일로 저장하기
 		private void save() {
 			try {
 				File file = new File("data");
@@ -221,10 +218,30 @@ public class ApplyService {
 				e.printStackTrace();
 			}
 		}
-	
-	
-	
-	
+		
+		
+		
+		
+		
+		
+		
+//		============================= 주석처리 ==============================
+//		// 로그인한 사용자가 이력서들
+//		List<Resume> resumes = ResumeService.getInstance().findResumeBy(UserService.getInstance().getLoginUser().getUserNo());
+//		// 해당 이력서들로 지원한 지원서들
+//		List<Apply> applies = new ArrayList<>();
+//		for(Resume r : resumes) {
+//			applies.addAll(findApplysBy(r.getResumeNo()));
+//		}
+//		// 해당 지원서들에 해당되는 공고
+//		List<Gonggo> gonggoes = new ArrayList<>();
+//		for(Apply a : applies) {
+//			System.out.println("지원 시간 :" + dateFormat.format(a.getApplyDate()) + "\n지원상태 : " + (a.getApplySitu() == 0 ? "접수" : "읽음")); 
+//			gonggoes.addAll(GonggoService.getInstance().findGonggoBy(a.getGonggoNo()));
+//			System.out.println(gonggoes.get(gonggoes.size()-1).toString());
+//		}
+		
+		
 	
 	
 	
